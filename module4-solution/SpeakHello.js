5bb9a536c09b55ff3b94000786cfaeeefa948f1d
vinod0884 <71624143+vinod0884@users.noreply.github.com>
2021-05-27 07:20:31 +0530

    Delete module4-solution directory

--- module4-solution/SpeakHello.js
+++
@@ -1,8 +0,0 @@
-(function (window) {
-	var HelloGreeter = new Object ();
-    var greet = "Hello";
-    HelloGreeter.speakhello = function speakhello (name) {
-	console.log (greet + " " + name + "!")
-};
-   window.HelloGreeter = HelloGreeter;
-})(window);
